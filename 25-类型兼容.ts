a55583b6399a9807327ad52067cca8aa47967c65
kenny <2916363651@qq.com>
2024-06-24 17:36:43 +0800

    ss

---
+++ 25-类型兼容.ts
@@ -0,0 +1,23 @@
+interface A {
+  fn: (value: number | string) => string;
+}
+
+// interface B extends A {
+//     fn:(value:string)=>string
+// }
+
+// type TableListItem = {
+//   key: number;
+//   name: string;
+//   createdAt: string;
+// };
+
+// const columns: TableListItem[] = [];
+
+type P = {
+  name: string;
+  age: number;
+  address: string;
+};
+
+
